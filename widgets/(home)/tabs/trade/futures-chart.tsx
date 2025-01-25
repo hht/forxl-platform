542a53181e501557c5b0304b27c635f985974ee6
hht <hht@163.com>
2025-01-26 01:24:16 +0800

    feat: 点击产品名称直接显示K线图

--- widgets/(home)/tabs/trade/futures-chart.tsx
+++ widgets/(home)/tabs/trade/futures-chart.tsx
@@ -10,7 +10,6 @@ import { FutureChartWidget } from "~/widgets/shared/future-chart-widget"
 
 export const FuturesChart: FC = () => {
   const currentSymbol = useSymbolStore((state) => state.currentSymbol, shallow)
-  const [visible, setVisible] = useState(false)
   const [resolution, setResolution] = useState<string | number>(1)
   const { t } = useTranslation()
   const TIMES = useMemo(() => {
@@ -41,11 +40,6 @@ export const FuturesChart: FC = () => {
       },
     ]
   }, [t])
-  useEffect(() => {
-    if (!currentSymbol) {
-      setVisible(false)
-    }
-  }, [currentSymbol])
   return (
     <AnimatePresence>
       {currentSymbol && (
@@ -76,40 +70,38 @@ export const FuturesChart: FC = () => {
             <Text>{`${currentSymbol.symbol} ${t("trade.chart")}`}</Text>
             <XStack f={1} />
             <XStack
-              rotate={visible ? "90deg" : "270deg"}
+              rotate="90deg"
               hitSlop={16}
               onPress={() => {
-                setVisible((v) => !visible)
+                useSymbolStore.setState({ currentSymbol: undefined })
               }}
             >
               <Icon name="chevronRight" size={16}></Icon>
             </XStack>
           </XStack>
-          {visible ? (
-            <XStack
-              p="$md"
-              bc="$background"
-              btc="$border"
-              btw={1}
-              ai="center"
-              gap="$md"
-            >
-              {TIMES.map((time) => (
-                <XStack
-                  key={time.value}
-                  hitSlop={16}
-                  onPress={() => setResolution(time.value)}
+          <XStack
+            p="$md"
+            bc="$background"
+            btc="$border"
+            btw={1}
+            ai="center"
+            gap="$md"
+          >
+            {TIMES.map((time) => (
+              <XStack
+                key={time.value}
+                hitSlop={16}
+                onPress={() => setResolution(time.value)}
+              >
+                <Text
+                  col={resolution === time.value ? "$primary" : "$secondary"}
                 >
-                  <Text
-                    col={resolution === time.value ? "$primary" : "$secondary"}
-                  >
-                    {time.label}
-                  </Text>
-                </XStack>
-              ))}
-            </XStack>
-          ) : null}
-          {currentSymbol && visible && (
+                  {time.label}
+                </Text>
+              </XStack>
+            ))}
+          </XStack>
+          {currentSymbol && (
             <FutureChartWidget
               futuresCode={currentSymbol.symbol}
               volatility={currentSymbol.volatility}
