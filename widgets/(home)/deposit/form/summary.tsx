6854ea80c71c230952951bc186659f23aadfe183
hht <hht@163.com>
2025-04-09 22:12:58 +0800

    fix: 手续费计算

--- widgets/(home)/deposit/form/summary.tsx
+++ widgets/(home)/deposit/form/summary.tsx
@@ -1,10 +1,11 @@
-import { FC, Fragment } from "react"
-import { useTranslation } from "react-i18next"
+import { floor } from 'lodash'
+import { FC, Fragment } from 'react'
+import { useTranslation } from 'react-i18next'
 
-import { Card, Separator, Text, YStack } from "~/components"
-import { useWalletStore } from "~/hooks/useStore"
-import { formatCurrency } from "~/lib/utils"
-import { PaymentMethodDescription } from "~/widgets/shared/payment-methods"
+import { Card, Separator, Text, YStack } from '~/components'
+import { useWalletStore } from '~/hooks/useStore'
+import { formatCurrency } from '~/lib/utils'
+import { PaymentMethodDescription } from '~/widgets/shared/payment-methods'
 
 export const DepositSummary: FC = () => {
   const { depositMethod: method, depositRequest } = useWalletStore()
@@ -12,14 +13,14 @@ export const DepositSummary: FC = () => {
   if (!method) return null
   return (
     <Card gap={12}>
-      {method.payType === 3 ? (
+      {method.payType === 3 || method.payType === 101 ? (
         <Fragment>
           <YStack gap="$sm">
             <Text col="$secondary">{t("wallet.youWillGet")}</Text>
             <Text subject>
               {formatCurrency(
                 depositRequest?.amount
-                  ? depositRequest.amount - (method.fee ?? 0)
+                  ? depositRequest.amount * (1 - (method.fee ?? 0))
                   : 0
               )}
             </Text>
