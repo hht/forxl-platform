6c45fbeeae26483ace8fc61f346439cb79a3b6a3
hht <hht@163.com>
2025-05-03 09:48:35 +0800

    feat: 处理邀请码逻辑

--- widgets/shared/detector.tsx
+++ widgets/shared/detector.tsx
@@ -1,9 +1,35 @@
 import { router, useNavigationContainerRef, useRootNavigationState } from 'expo-router'
 import { useEffect } from 'react'
 
+import { validateInviteCode } from '~/api/account'
+import { useStore } from '~/app/(anon)/auth/sign-up'
 import { useLocaleCalendar } from '~/hooks/useLocale'
 import { useForxlStore } from '~/hooks/useStore'
 import { dismissAll } from '~/lib/utils'
+
+const validateCode = async (code: string) => {
+  if (!code || !/^[A-Za-z0-9]{8}$/.test(code)) {
+    return false
+  }
+  try {
+    const validated = await validateInviteCode(code)
+    if (validated) {
+      useStore.setState({
+        validated,
+        inviteCode: code,
+      })
+      return true
+    } else {
+      useStore.setState({
+        validated: false,
+        inviteCode: '',
+      })
+      return false
+    }
+  } catch (error) {
+    return false
+  }
+}
 
 export const AccountDetector = () => {
   const userNumber = useForxlStore((state) => state.userNumber)
@@ -11,13 +37,24 @@ export const AccountDetector = () => {
   const navigationState = useRootNavigationState()
   useEffect(() => {
     if (!navigationState?.key || !rootNavigation.current) return
-    requestAnimationFrame(() => {
-      if (userNumber && (rootNavigation.current?.getCurrentRoute()?.name === 'index' || rootNavigation.current?.getCurrentRoute()?.name.startsWith('auth'))) {
+    requestAnimationFrame(async () => {
+      const currentRoute = rootNavigation.current?.getCurrentRoute()
+      if (!currentRoute) return
+      if (currentRoute.name === 'index' && currentRoute.params) {
+        const params = currentRoute.params as any
+        if (params?.code) {
+          const validated = await validateCode((params).code as string)
+          if (validated) {
+            router.replace("/auth/sign-up")
+          }
+        }
+      }
+      if (userNumber && (currentRoute?.name === 'index' || currentRoute?.name.startsWith('auth'))) {
         dismissAll()
         router.replace("/tabs/dashboard")
         return
       }
-      if (!userNumber && !["index", 'auth/sign-in/index'].includes(rootNavigation.current?.getCurrentRoute()?.name ?? '')) {
+      if (!userNumber && !["index", 'auth/sign-in/index', 'auth/sign-up/index', 'create-password/index', 'forgot-password/index', 'reset-password/index', 'verify-email/index'].includes(currentRoute?.name ?? '')) {
         dismissAll()
         router.replace("/")
       }
